ethereum-optimism/optimism

da: use celestia-openrpc (#187)
* op-node: use openrpc to get blobs

* op-batcher: use openrpc to submit blobs

* op-celestia-: use openrpc to fetch blobs

* da: update rpc port

* docker: update rpc port

* readme: devnet - rm unused port

* docker: devnet - restore platform

op-node/service.go
@@ -57,7 +57,11 @@ func NewConfig(ctx *cli.Context, log log.Logger) (*node.Config, error) {
 
 	l2SyncEndpoint := NewL2SyncEndpointConfig(ctx)
 
-	daCfg, err := rollup.NewDAConfig(ctx.GlobalString(flags.DaRPC.Name), ctx.GlobalString(flags.NamespaceId.Name))
+	daCfg, err := rollup.NewDAConfig(
+		ctx.GlobalString(flags.DaRPC.Name),
+		ctx.GlobalString(flags.AuthToken.Name),
+		ctx.GlobalString(flags.NamespaceId.Name),
+	)
 	if err != nil {
 		return nil, fmt.Errorf("failed to load da config: %w", err)
 	}

op-service/txmgr/cli.go
@@ -33,6 +33,7 @@ const (
 	ReceiptQueryIntervalFlagName      = "txmgr.receipt-query-interval"
 	DaRpcFlagName                     = "da-rpc"
 	NamespaceIdFlagName               = "namespace-id"
+	AuthTokenFlagName                 = "auth-token"
 )
 
 var (
@@ -117,6 +118,12 @@ func CLIFlags(envPrefix string) []cli.Flag {
 			Value:  "000008e5f679bf7116cb",
 			EnvVar: opservice.PrefixEnvVar(envPrefix, "NAMESPACE_ID"),
 		},
+		cli.StringFlag{
+			Name:   AuthTokenFlagName,
+			Usage:  "Authentication Token of the DA layer",
+			Value:  "",
+			EnvVar: opservice.PrefixEnvVar(envPrefix, "AUTH_TOKEN"),
+		},
 	}, client.CLIFlags(envPrefix)...)
 }
 
@@ -137,6 +144,7 @@ type CLIConfig struct {
 	TxNotInMempoolTimeout     time.Duration
 	DaRpc                     string
 	NamespaceId               string
+	AuthToken                 string
 }
 
 func (m CLIConfig) Check() error {
@@ -185,6 +193,7 @@ func ReadCLIConfig(ctx *cli.Context) CLIConfig {
 		TxNotInMempoolTimeout:     ctx.GlobalDuration(TxNotInMempoolTimeoutFlagName),
 		DaRpc:                     ctx.GlobalString(DaRpcFlagName),
 		NamespaceId:               ctx.GlobalString(NamespaceIdFlagName),
+		AuthToken:               ctx.GlobalString(AuthTokenFlagName),
 	}
 }
 
@@ -278,6 +287,9 @@ type Config struct {
 
 	// NamespaceId is the id of the namespace of the Data Availability node.
 	NamespaceId string
+
+	// AuthToken is the authentication token for the Data Availability node.
+	AuthToken string
 
 	// Signer is used to sign transactions when the gas price is increased.
 	Signer opcrypto.SignerFn

Manta-Network/celestia-bedrock-manta

op-node/service.go
@@ -58,7 +58,11 @@ func NewConfig(ctx *cli.Context, log log.Logger) (*node.Config, error) {
 
 	l2SyncEndpoint := NewL2SyncEndpointConfig(ctx)
 
-	daCfg, err := rollup.NewDAConfig(ctx.String(flags.DaRPC.Name), ctx.String(flags.NamespaceId.Name))
+	daCfg, err := rollup.NewDAConfig(
+		ctx.String(flags.DaRPC.Name),
+		ctx.String(flags.AuthToken.Name),
+		ctx.String(flags.NamespaceId.Name),
+	)
 	if err != nil {
 		return nil, fmt.Errorf("failed to load da config: %w", err)
 	}

op-service/txmgr/cli.go
@@ -34,6 +34,7 @@ const (
 	ReceiptQueryIntervalFlagName      = "txmgr.receipt-query-interval"
 	DaRpcFlagName                     = "da-rpc"
 	NamespaceIdFlagName               = "namespace-id"
+	AuthTokenFlagName                 = "auth-token"
 )
 
 var (
@@ -120,6 +121,12 @@ func CLIFlags(envPrefix string) []cli.Flag {
 			Value:  "000008e5f679bf7116cb",
 			EnvVars: prefixEnvVars("NAMESPACE_ID"),
 		},
+		&cli.StringFlag{
+			Name:   AuthTokenFlagName,
+			Usage:  "Authentication Token of the DA layer",
+			Value:  "",
+			EnvVars: prefixEnvVars("AUTH_TOKEN"),
+		},
 	}, signerFlags...)
 }
 
@@ -141,6 +148,7 @@ type CLIConfig struct {
 	TxNotInMempoolTimeout     time.Duration
 	DaRpc                     string
 	NamespaceId               string
+	AuthToken                 string
 }
 
 func (m CLIConfig) Check() error {
@@ -193,6 +201,7 @@ func ReadCLIConfig(ctx *cli.Context) CLIConfig {
 		TxNotInMempoolTimeout:     ctx.Duration(TxNotInMempoolTimeoutFlagName),
 		DaRpc:                     ctx.String(DaRpcFlagName),
 		NamespaceId:               ctx.String(NamespaceIdFlagName),
+		AuthToken:                 ctx.String(AuthTokenFlagName),
 	}
 }
 
@@ -304,6 +313,9 @@ type Config struct {
 
 	// NamespaceId is the id of the namespace of the Data Availability node.
 	NamespaceId string
+
+	// AuthToken is the authentication token for the Data Availability node.
+	AuthToken string
 
 	// Signer is used to sign transactions when the gas price is increased.
 	Signer opcrypto.SignerFn